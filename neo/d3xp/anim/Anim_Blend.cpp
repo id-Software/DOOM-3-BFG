98093a4e8dd1795e04563bd2ae3a45c0a5ce24c5
Robert Beckebans <robert.beckebans@gmail.com>
2013-01-14 00:24:11 +0100

    Added long neck GCC compiler workaround from dhewm3

--- neo/d3xp/anim/Anim_Blend.cpp
+++ neo/d3xp/anim/Anim_Blend.cpp
@@ -5490,18 +5490,20 @@ bool idAnimator::GetJointLocalTransform( jointHandle_t jointHandle, int currentT
 	// FIXME: overkill
 	CreateFrame( currentTime, false );
 	
-	if( jointHandle > 0 )
-	{
-		idJointMat m = joints[ jointHandle ];
-		m /= joints[ modelJoints[ jointHandle ].parentNum ];
-		offset = m.ToVec3();
-		axis = m.ToMat3();
-	}
-	else
+	// RB: long neck GCC compiler bug workaround from dhewm3 ...
+	if( jointHandle == 0 )
 	{
 		offset = joints[ jointHandle ].ToVec3();
 		axis = joints[ jointHandle ].ToMat3();
+		
+		return true;
 	}
+	
+	idJointMat m = joints[ jointHandle ];
+	m /= joints[ modelJoints[ jointHandle ].parentNum ];
+	offset = m.ToVec3();
+	axis = m.ToMat3();
+	// RB end
 	
 	return true;
 }
