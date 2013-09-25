9e7230de34af66bd9b6e35ac93802173d4d6be14
Robert Beckebans <robert.beckebans@gmail.com>
2013-09-25 09:19:21 +0200

    Disabled tunnel vision. Fixes #33 Red screen flash on damage does not dissapear until load or full health

--- neo/d3xp/PlayerView.cpp
+++ neo/d3xp/PlayerView.cpp
@@ -516,6 +516,10 @@ void idPlayerView::SingleView( const renderView_t* view, idMenuHandler_HUD* hudM
 		
 		
 		// tunnel vision
+		/*
+		RB: disabled tunnel vision because the renderer converts colors set by SetColor4 to bytes which are clamped to [0,255]
+		so materials that want to access float values greater than 1 with parm0 - parm3 are always broken
+		
 		float health = 0.0f;
 		if( g_testHealthVision.GetFloat() != 0.0f )
 		{
@@ -540,6 +544,8 @@ void idPlayerView::SingleView( const renderView_t* view, idMenuHandler_HUD* hudM
 			renderSystem->SetColor4( ( player->health <= 0.0f ) ? MS2SEC( gameLocal.slow.time ) : lastDamageTime, 1.0f, 1.0f, ( player->health <= 0.0f ) ? 0.0f : alpha );
 			renderSystem->DrawStretchPic( 0.0f, 0.0f, renderSystem->GetVirtualWidth(), renderSystem->GetVirtualHeight(), 0.0f, 0.0f, 1.0f, 1.0f, tunnelMaterial );
 		}
+		RB end
+		*/
 		
 		if( bfgVision )
 		{
