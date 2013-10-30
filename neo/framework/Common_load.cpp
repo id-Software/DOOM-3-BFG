a92a49dc2f4b8cdbbaed0e223a69fed35a73dbb2
Robert Beckebans <robert.beckebans@gmail.com>
2013-10-30 09:42:24 +0100

    Astyle

--- neo/framework/Common_load.cpp
+++ neo/framework/Common_load.cpp
@@ -1051,12 +1051,12 @@ HandleCommonErrors
 bool HandleCommonErrors( const idSaveLoadParms& parms )
 {
 	common->Dialog().ShowSaveIndicator( false );
-
+	
 	if( parms.GetError() == SAVEGAME_E_NONE )
 	{
 		return true;
 	}
-
+	
 	if( parms.GetError() & SAVEGAME_E_CORRUPTED )
 	{
 		// This one might need to be handled by the game
