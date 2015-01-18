c0f537ae7e5160977e82ae22a0c9189b619ada35
Robert Beckebans <robert.beckebans@gmail.com>
2015-01-18 12:02:48 +0100

    Made SDL 2 the default

--- neo/cmake-linux-release.sh
+++ neo/cmake-linux-release.sh
@@ -2,4 +2,4 @@ cd ..
 rm -rf build
 mkdir build
 cd build
-cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DONATIVE=ON ../neo
+cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DONATIVE=ON -DSDL2=ON ../neo
