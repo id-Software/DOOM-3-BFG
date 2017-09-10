b3637e8fde3a9e49d621f19d29efebbfe07bb76a
Robert Beckebans <robert.beckebans@gmail.com>
2017-09-10 13:43:28 +0200

    Moved tr_backend_rendertools.cpp

--- neo/renderer/OpenGL/RenderBackend_GL.cpp
+++ neo/renderer/OpenGL/RenderBackend_GL.cpp
@@ -4,6 +4,7 @@
 Doom 3 BFG Edition GPL Source Code
 Copyright (C) 1993-2012 id Software LLC, a ZeniMax Media company.
 Copyright (C) 2013-2015 Robert Beckebans
+Copyright (C) 2016-2017 Dustin Land
 
 This file is part of the Doom 3 BFG Edition GPL Source Code ("Doom 3 BFG Edition Source Code").
 
