cc3f1a78d3a2b9f62d07612b468515f51857f986
Robert Beckebans <robert.beckebans@gmail.com>
2019-11-11 21:02:52 +0100

    Applied new Astyle settings to ImGui

--- neo/astyle-code.bat
+++ neo/astyle-code.bat
@@ -1,5 +1,9 @@
 astyle.exe -v --formatted --options=astyle-options.ini --exclude="libs" --recursive *.h
 astyle.exe -v --formatted --options=astyle-options.ini --exclude="libs" --exclude="d3xp/gamesys/SysCvar.cpp" --exclude="d3xp/gamesys/Callbacks.cpp" --exclude="sys/win32/win_cpu.cpp" --exclude="sys/win32/win_main.cpp" --recursive *.cpp
+
+astyle.exe -v --formatted --options=astyle-options.ini --recursive libs/imgui/*.h
+astyle.exe -v --formatted --options=astyle-options.ini --recursive libs/imgui/*.cpp
+
 astyle.exe -v -Q --options=astyle-options.ini ../base/renderprogs/postprocess.ps.hlsl
 astyle.exe -v -Q --options=astyle-options.ini ../base/renderprogs/AmbientOcclusion_AO.ps.hlsl
 astyle.exe -v -Q --options=astyle-options.ini ../base/renderprogs/AmbientOcclusion_blur.ps.hlsl
