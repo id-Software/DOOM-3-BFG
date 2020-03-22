98630cf6ecf0a344e640d5dd072ae323447a9c21
Robert Beckebans <robert.beckebans@gmail.com>
2020-03-22 12:27:27 +0100

    Deleted cmake configs for VS 2010 and 2012

--- neo/cmake-vs2010-32bit.bat
+++
@@ -1,6 +0,0 @@
-cd ..
-del /s /q build
-mkdir build
-cd build
-cmake -G "Visual Studio 10" -DCMAKE_INSTALL_PREFIX=../bin/win32 ../neo
-pause
--- neo/cmake-vs2012-32bit.bat
+++
@@ -1,6 +0,0 @@
-cd ..
-del /s /q build
-mkdir build
-cd build
-cmake -G "Visual Studio 11" -DCMAKE_INSTALL_PREFIX=../bin/win8-32 ../neo
-pause
