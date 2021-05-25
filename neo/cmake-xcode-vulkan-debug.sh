2068e92d92c344c1111a8ba257be5a9ce5a29059
Stephen Saunders <srsaunders@rogers.com>
2021-05-25 13:33:28 -0400

    Use default VULKAN_SDK location on macOS; disable broken ZERO_CHECK for Xcode Vulkan builds

--- neo/cmake-xcode-vulkan-debug.sh
+++ neo/cmake-xcode-vulkan-debug.sh
@@ -2,4 +2,4 @@ cd ..
 rm -rf xcode-vulkan-debug
 mkdir xcode-vulkan-debug
 cd xcode-vulkan-debug
-cmake -G Xcode -DCMAKE_BUILD_TYPE=Debug -DSDL2=ON -DUSE_VULKAN=ON -DSPIRV_SHADERC=OFF -DOPENAL_LIBRARY=/usr/local/opt/openal-soft/lib/libopenal.dylib -DOPENAL_INCLUDE_DIR=/usr/local/opt/openal-soft/include ../neo -Wno-dev
+cmake -G Xcode -DCMAKE_SUPPRESS_REGENERATION=ON -DCMAKE_BUILD_TYPE=Debug -DSDL2=ON -DUSE_VULKAN=ON -DSPIRV_SHADERC=OFF -DOPENAL_LIBRARY=/usr/local/opt/openal-soft/lib/libopenal.dylib -DOPENAL_INCLUDE_DIR=/usr/local/opt/openal-soft/include ../neo -Wno-dev
