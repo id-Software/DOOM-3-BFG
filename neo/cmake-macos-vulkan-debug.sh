ab9857987c6114b3503cc08f4e70771ffc7f6666
Stephen Saunders <srsaunders@rogers.com>
2021-06-10 12:33:17 -0400

    Add note to macOS vulkan build script re MVK_CONFIG_FULL_IMAGE_VIEW_SWIZZLE env var

--- neo/cmake-macos-vulkan-debug.sh
+++ neo/cmake-macos-vulkan-debug.sh
@@ -3,3 +3,4 @@ rm -rf build
 mkdir build
 cd build
 cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug -DSDL2=ON -DUSE_VULKAN=ON -DSPIRV_SHADERC=OFF -DOPENAL_LIBRARY=/usr/local/opt/openal-soft/lib/libopenal.dylib -DOPENAL_INCLUDE_DIR=/usr/local/opt/openal-soft/include ../neo -Wno-dev
+echo "** NOTE: For this build type must set environment variable MVK_CONFIG_FULL_IMAGE_VIEW_SWIZZLE=1 at runtime **"
