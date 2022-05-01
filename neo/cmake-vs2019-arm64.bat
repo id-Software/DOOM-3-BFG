c6735ce1dfea2d390a75d50461fd2a7d219ef216
Robert Beckebans <robert.beckebans@gmail.com>
2022-05-01 21:36:06 +0200

    Tweaked presets for Windows ARM

--- neo/cmake-vs2019-arm64.bat
+++ neo/cmake-vs2019-arm64.bat
@@ -2,5 +2,5 @@ cd ..
 del /s /q build
 mkdir build
 cd build
-cmake -G "Visual Studio 16" -A arm64 -D WINDOWS10=ON -D USE_INTRINSICS_SSE=OFF ../neo 
+cmake -G "Visual Studio 16" -A arm64 -D WINDOWS10=ON -D USE_INTRINSICS_SSE=OFF -D FFMPEG=OFF -D BINKDEC=ON ../neo 
 pause
