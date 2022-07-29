7544ae17188f166e6e0d2dc6b11ca9d84afd6078
Trung Le <trung.le@ruby-joural.com>
2022-07-29 23:34:30 +1000

    Fix GCC12 warning on sprintf()

--- doomclassic/doom/hu_stuff.cpp
+++ doomclassic/doom/hu_stuff.cpp
@@ -327,7 +327,7 @@ void HU_Init(void)
 	j = HU_FONTSTART;
 	for (i=0;i<HU_FONTSIZE;i++)
 	{
-		sprintf(buffer, "STCFN%.3d", j++);
+		snprintf(buffer, strlen(buffer), "STCFN%.3d", j++);
 		::g->hu_font[i] = (patch_t *) W_CacheLumpName(buffer, PU_STATIC_SHARED);
 	}
 
