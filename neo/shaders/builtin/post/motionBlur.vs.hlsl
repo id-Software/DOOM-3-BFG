c1c0334bf2107896bbf75c2d9c37e9675bb1ebfa
Stephen Pridham <stephenap07@gmail.com>
2022-11-18 08:00:13 -0500

    Fix VK descriptor set slot for motionBlur shader

--- neo/shaders/builtin/post/motionBlur.vs.hlsl
+++ neo/shaders/builtin/post/motionBlur.vs.hlsl
@@ -26,9 +26,6 @@ If you have questions concerning this license or the applicable additional terms
 ===========================================================================
 */
 
-#include "global_inc.hlsl"
-
-
 // *INDENT-OFF*
 struct VS_IN
 {
