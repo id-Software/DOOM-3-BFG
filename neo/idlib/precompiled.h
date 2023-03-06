2ad666adcff9d6a56616d10f90ff1c47265adc97
Robert Beckebans <robert.beckebans@gmail.com>
2023-03-06 20:31:54 +0100

    NUM_FRAME_DATA cleanup

--- neo/idlib/precompiled.h
+++ neo/idlib/precompiled.h
@@ -90,13 +90,8 @@ const int MAX_EXPRESSION_REGISTERS = 4096;
 // everything that is needed by the backend needs
 // to be double buffered to allow it to run in
 // parallel on a dual cpu machine
-#if ( defined(__APPLE__) && defined( USE_VULKAN ) ) || defined( USE_NVRHI )
-	// SRS - macOS MoltenVK/Metal needs triple buffering for full screen to work properly
-	// SRS - use triple buffering for NVRHI with command queue event query sync method
-	const uint32 NUM_FRAME_DATA	= 3;
-#else
-	const uint32 NUM_FRAME_DATA = 2;
-#endif
+// SRS - use triple buffering for NVRHI with command queue event query sync method
+const uint32 NUM_FRAME_DATA	= 3;
 
 #if defined( USE_NVRHI )
 	#include "nvrhi/nvrhi.h"
