a5a6bf0e33d985a2af47b8db2a000200617179c5
Robert Beckebans <robert.beckebans@gmail.com>
2023-03-14 10:33:41 +0100

    Format doomclassic with Astyle too

--- neo/astyle-code.bat
+++ neo/astyle-code.bat
@@ -1,6 +1,9 @@
 astyle.exe -v --formatted --options=astyle-options.ini --exclude="libs" --exclude="extern" --recursive *.h
 astyle.exe -v --formatted --options=astyle-options.ini --exclude="libs" --exclude="extern" --exclude="d3xp/gamesys/SysCvar.cpp" --exclude="d3xp/gamesys/Callbacks.cpp" --exclude="sys/win32/win_cpu.cpp" --recursive *.cpp
 
+astyle.exe -v --formatted --options=astyle-options.ini --recursive ../doomclassic/*.h
+astyle.exe -v --formatted --options=astyle-options.ini --recursive ../doomclassic/*.cpp
+
 REM astyle.exe -v --formatted --options=astyle-options.ini --recursive libs/imgui/*.h
 REM astyle.exe -v --formatted --options=astyle-options.ini --recursive libs/imgui/*.cpp
 
@@ -9,8 +12,6 @@ REM astyle.exe -v --formatted --options=astyle-options.ini --recursive libs/tiny
 
 REM astyle.exe -v --formatted --options=astyle-options.ini --recursive libs/oggvorbis/*.h
 REM astyle.exe -v --formatted --options=astyle-options.ini --recursive libs/oggvorbis/*.c
-
-astyle.exe -v -Q --options=astyle-options.ini --recursive ../base/renderprogs/*.hlsl
 
 astyle.exe -v -Q --options=astyle-options.ini --recursive shaders/*.hlsl
 
